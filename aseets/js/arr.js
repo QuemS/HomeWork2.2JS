816ecdf8e4e5bdfaa1421be94ff834cb23f233cf
Sushchenok Mykhailo <hostkillcar@gmail.com>
2022-01-25 11:36:39 +0200

    v 1 -999

---
+++ aseets/js/arr.js
@@ -0,0 +1,58 @@
+let arrOne = [
+    {simbol: '1', icao: 'oдна'},
+    {simbol: '2', icao: 'дві'},
+    {simbol: '3', icao: 'три'},
+    {simbol: '4', icao: 'чотирі'},
+    {simbol: '5', icao: "п'ять"},
+    {simbol: '6', icao: 'шість'},
+    {simbol: '7', icao: 'сім'},
+    {simbol: '8', icao: 'вісім'},
+    {simbol: '9', icao: "дев'ять"},
+  
+    
+];
+
+let arrTen = [
+    {simbol: '0', icao: 'десять'},
+    {simbol: '1', icao: 'oдинадцять'},
+    {simbol: '2', icao: 'дванадцять'},
+    {simbol: '3', icao: 'тринадцять'},
+    {simbol: '4', icao: "чотирнадцять"},
+    {simbol: '5', icao: "п'ятнадцять"},
+    {simbol: '6', icao: 'шістнадцять'},
+    {simbol: '7', icao: 'сімнадцять'},
+    {simbol: '8', icao: "вісімнадцять"},
+    {simbol: '9', icao: "дев'ятнадцять"},
+];
+
+let arrTwo = [
+  
+    {simbol: '2', icao: "двадцять"},
+    {simbol: '3', icao: "тридцять"},
+    {simbol: '4', icao: "сорок"},
+    {simbol: '5', icao: "п'ятдесят"},
+    {simbol: '6', icao: "шістдесят"},
+    {simbol: '7', icao: "сімдесят"},
+    {simbol: '8', icao: "вісімдесят"},
+    {simbol: '9', icao: "дев'яносто"},
+    
+
+
+  
+    
+];
+
+let arrThree = [
+    {simbol: '1', icao: "сто"},
+    {simbol: '2', icao: "двісті"},
+    {simbol: '3', icao: "триста"},
+    {simbol: '4', icao: "чотириста"},
+    {simbol: '5', icao: "пʼятсот"},
+    {simbol: '6', icao: "шістсот"},
+    {simbol: '7', icao: "сімсот"},
+    {simbol: '8', icao: "вісімсот"},
+    {simbol: '9', icao: "девʼятсот"},
+];
+
+
+export{arrOne,arrTwo,arrThree,arrTen};
